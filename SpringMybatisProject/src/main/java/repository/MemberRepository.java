1d7d2ff917a85b877ec2334da173a3d7d26513f4
1 <1@EJY-Laptop>
2021-07-05 22:12:44 +0900

    210705_pm1012

---
+++ SpringMybatisProject/src/main/java/repository/MemberRepository.java
@@ -0,0 +1,33 @@
+package repository;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import Model.MemberDTO;
+
+public class MemberRepository {
+	@Autowired
+	SqlSession sqlSession;
+	
+	String namespace = "mappers.memberMapper";
+	String statement;
+	
+	public void memDel(String memId) {
+		statement = namespace +".memDel";
+		sqlSession.delete(statement, memId);
+	}
+	public void memUpdate(MemberDTO dto) {
+		statement = namespace + ".memUpdate";
+		sqlSession.update(statement, dto);
+	}
+	public List<MemberDTO> memList(String memId) {
+		statement = namespace +".memList";
+		return sqlSession.selectList(statement,memId);
+	}
+	public void memJoin(MemberDTO dto) {
+		statement = namespace + ".memJoin";
+		sqlSession.insert(statement, dto);
+	}
+}
